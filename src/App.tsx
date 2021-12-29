c7cdd49ec540c498c718cf2ea925fa4b469d9f9d
rodrigonahid <rodrigonahid1@gmail.com>
2021-12-29 20:03:48 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+
+const Container = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalStyle>
+      <Container>
+        <header className="App-header">
+          <p>AAAAAAAAAAAAAAAA</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </Container>
+    </GlobalStyle>
   );
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --red: #E52E4D;
+        --green: #33CC95;
+        --blue: #5429CC;
+        --blue-light: #6933FF;
+
+        --shape: #FFFFFF;
+        
+        --title: #363F5F;
+        --text: #969CB2;
+        --background: #F0F2F5;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media(max-width: 1080px){
+            font-size: 93.75%;
+        }
+        @media(max-width: 720px){
+            font-size: 87.5%;
+        }
+    }
+
+    body{
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    button {
+        cursor: pointer;
+    }
+    disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
